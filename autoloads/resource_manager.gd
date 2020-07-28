15e01f225cd9125f8a2935103621466da36c0a80
Mick Veldhuis <mickveldhuis@hotmail.nl>
2020-07-28 17:33:38 +0200

    Update the project structure

---
+++ autoloads/resource_manager.gd
@@ -0,0 +1,39 @@
+extends Node
+
+onready var sprites = {
+	"mouse_default": preload("res://ui/common_assets/cursor.png"),
+	"selector_ok": preload("res://ui/build_menu/assets/selector_green.png"),
+	"selector_error": preload("res://ui/build_menu/assets/selector_red.png"),
+}
+onready var components = {
+	"tile_selector": preload("res://ui/build_menu/tile_selector.tscn"),
+}
+onready var placeables = {
+	"houses": {
+		"wood": preload("res://entities/buildings/house.tscn"),
+	},
+	"infrastructure": {
+		
+	},
+}
+onready var placeable_sprites = {
+	"houses": {
+		"wood": preload("res://entities/buildings/assets/house_wood.png"),
+	},
+	"infrastructure": {
+		
+	},
+}
+onready var tools = {
+	Global.ToolType.AXE: preload("res://ui/inventory/resources/tools/axe.tres"),
+	Global.ToolType.PICKAXE: preload("res://ui/inventory/resources/tools/pickaxe.tres"),
+}
+onready var items = {
+	Global.ItemType.EMPTY: preload("res://ui/inventory/resources/items/empty_item.tres"),
+	Global.ItemType.WOOD: preload("res://ui/inventory/resources/items/wood.tres"),
+	Global.ItemType.STONE: preload("res://ui/inventory/resources/items/stone.tres"),
+}
+onready var pickups = {
+	Global.ItemType.WOOD: preload("res://entities/pickups/wood_pickup.tscn"),
+	Global.ItemType.STONE: preload("res://entities/pickups/stone_pickup.tscn"),
+}
